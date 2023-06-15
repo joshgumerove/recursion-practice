f5b4decd0c5278b4d4316a03855acdea804b1608
Josh Gumerove <jgumerove@kaufmanrossin.com>
2023-06-15 03:51:57 -0400

    memoize fibonacci sequence

---
+++ optimizing-recursion/index.py
@@ -0,0 +1,16 @@
+# without memoization --> repeating function calls uselessly
+# using memoization --> produces a smaller recursion tree (reduces time complexity)
+
+def fib_memoized(n, lookup={}):
+    key = str(n)
+    if key in lookup:
+        return lookup[key]
+    if n <= 1:
+        lookup[key] = n
+        return lookup[key]
+    
+    ans = fib_memoized(n - 1) + fib_memoized(n -2)
+    lookup[key] = ans
+    return ans
+
+print(fib_memoized(3))
